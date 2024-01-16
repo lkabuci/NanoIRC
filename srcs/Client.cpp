fc6326a3b769c72b04ed4dcb1b2ba6efcb529177
Greck <robby947@wireconnected.com>
2024-01-16 14:02:37 +0100

    add client class

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Client.hpp"
+#include "ServerHelper.hpp"
+
+Client::Client(sockaddr_storage &sockaddr, int sockfd)
+    : _sockAddr(sockaddr), _sockfd(sockfd), _ip(), _port(),
+      _isDoneReading(false) {
+    fillClientIpPort();
+    std::cout << "+ add client: (" << sockfd << ") \"" << _ip << "\""
+              << std::endl;
+}
+
+int Client::getSockfd() const {
+    return _sockfd;
+}
+
+Client::~Client() {
+    std::cout << "- removing client: (" << _sockfd << ") \"" << _ip << "\""
+              << std::endl;
+    close(_sockfd);
+}
+
+void Client::fillClientIpPort() {
+    struct sockaddr_in  *ipv4 = NULL;
+    struct sockaddr_in6 *ipv6 = NULL;
+
+    std::memset(_ip, 0, sizeof _ip);
+    std::memset(_port, 0, sizeof _port);
+
+    switch (_sockAddr.ss_family) {
+    case AF_INET:
+        ipv4 = reinterpret_cast<struct sockaddr_in *>(&_sockAddr);
+        inet_ntop(AF_INET, &(ipv4->sin_addr), _ip, sizeof _ip);
+        std::snprintf(_port, sizeof(_port), "%d", ntohs(ipv4->sin_port));
+        break;
+    case AF_INET6:
+        ipv6 = reinterpret_cast<struct sockaddr_in6 *>(&_sockAddr);
+        inet_ntop(AF_INET6, &(ipv6->sin6_addr), _ip, sizeof _ip);
+        std::snprintf(_port, sizeof(_port), "%d", ntohs(ipv6->sin6_port));
+        break;
+    default:
+        // TODO: here I should probably return a -1 or throw an error so the
+        // poll multiplexing ignore it
+        std::cerr << "Unknown address family\n";
+        break;
+    }
+}
+
+const UserInfo &Client::getUserInfo() const {
+    return _userInfo;
+}
+
+void Client::setUserInfo(const UserInfo &userInfo) {
+    _userInfo = userInfo;
+}
+
+const std::string &Client::getMessage() const {
+    return _message;
+}
+
+void Client::appendMessage(const std::string &message) {
+    _message.append(message, message.length());
+}
+
+bool Client::isDoneReading() const {
+    return _isDoneReading;
+}
+
+void Client::setIsDoneReading(bool isDoneReading) {
+    _isDoneReading = isDoneReading;
+}
--- srcs/Client.hpp
+++ srcs/Client.hpp
@@ -5,10 +5,32 @@
 #ifndef IRC_CLIENT_HPP
 #define IRC_CLIENT_HPP
 
+#include "../include/IRC.h"
+#include "UserInfo.hpp"
+
+#define PORT_LEN 6
 
 class Client {
+  public:
+    explicit Client(struct sockaddr_storage &, int);
+    ~Client();
+    int                getSockfd() const;
+    const UserInfo    &getUserInfo() const;
+    void               setUserInfo(const UserInfo &userInfo);
+    const std::string &getMessage() const;
+    void               appendMessage(const std::string &message);
+    bool               isDoneReading() const;
+    void               setIsDoneReading(bool isDoneReading);
 
+  private:
+    struct sockaddr_storage &_sockAddr;
+    int                      _sockfd;
+    char                     _ip[INET6_ADDRSTRLEN];
+    char                     _port[PORT_LEN];
+    UserInfo                 _userInfo;
+    void                     fillClientIpPort();
+    bool                     _isDoneReading;
+    std::string              _message;
 };
 
-
-#endif //IRC_CLIENT_HPP
+#endif // IRC_CLIENT_HPP
