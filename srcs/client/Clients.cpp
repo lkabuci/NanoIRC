445de16723f067bc9b0ff1b189f2fdcbac4ed9a5
Issam Abkadri <iabkadri@student.1337.ma>
2024-01-18 17:46:59 +0100

    add size() method

--- srcs/client/Clients.cpp
+++ srcs/client/Clients.cpp
@@ -12,6 +12,15 @@ Client* Clients::get(const std::string& nickname) {
     return (it == _clients.end()) ? NULL : it->second;
 }
 
+Client* Clients::get(const size_t& index) {
+    if (index >= _clients.size())
+        throw std::out_of_range("index out of range.");
+    std::map<std::string, Client*>::iterator it = _clients.begin();
+
+    std::advance(it, index);
+    return it->second;
+}
+
 void Clients::remove(const std::string& nickname) {
     std::map<std::string, Client*>::iterator it = _clients.find(nickname);
 
@@ -26,4 +35,8 @@ size_t Clients::numberOfClients() {
 
 bool Clients::exist(const std::string& nickname) {
     return _clients.find(nickname) != _clients.end();
+}
+
+size_t Clients::size() {
+    return _clients.size();
 }
--- srcs/client/Clients.hpp
+++ srcs/client/Clients.hpp
@@ -9,9 +9,11 @@ class Clients {
   public:
     static void    add(Client* client);
     static Client* get(const std::string& nickname);
+    static Client* get(const size_t& index);
     static void    remove(const std::string& nickname);
     static size_t  numberOfClients();
     static bool    exist(const std::string& nickname);
+    static size_t  size();
 
   private:
     Clients();
