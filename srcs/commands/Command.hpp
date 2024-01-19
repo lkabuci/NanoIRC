a68e49a68c53521e87abd113b62741b6e2d43b01
Issam Abkadri <iabkadri@student.1337.ma>
2024-01-19 19:37:59 +0100

    change Reply directory to reply

--- srcs/commands/Command.hpp
+++ srcs/commands/Command.hpp
@@ -3,11 +3,11 @@
 #ifndef __COMMAND_HPP__
 #define __COMMAND_HPP__
 
-#include "../Reply/Reply.hpp"
 #include "../Utils.hpp"
 #include "../channels/TChannels.hpp"
 #include "../client/ClientList.hpp"
 #include "../parser/Parser.hpp"
+#include "../reply/Reply.hpp"
 #include "../server/Reactor.hpp"
 
 class Command {
