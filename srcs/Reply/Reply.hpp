0e65dc873f70ab25a0b4e2f52925a574209f0a18
Issam Abkadri <iabkadri@student.1337.ma>
2024-01-20 23:06:05 +0100

    add errors description

--- srcs/Reply/Reply.hpp
+++ srcs/Reply/Reply.hpp
@@ -24,13 +24,17 @@ enum CODES {
     ERR_ERRONEUSNICKNAME = 432,
     //:<server> 433 <nickname> :Nickname is already in use
     ERR_NICKNAMEINUSE = 433,
+    //:<server> 403 <nickname> <channel> :No such channel
     ERR_NOSUCHCHANNEL = 403,
     ERR_INVITEONLYCHAN = 473,
     ERR_BADCHANNELKEY = 475,
+    //:<server> 411 <nickname> :No recipient given (PRIVMSG/NOTICE)
     ERR_NORECIPIENT = 411,
     //:<server> 412 <nickname> :No text to send
     ERR_NOTEXTTOSEND = 412,
+    //:<server> 407 <nickname> <command> :Too many targets
     ERR_TOOMANYTARGETS = 407,
+    //:<server> 401 <nickname> <target> :No such nickname
     ERR_NOSUCHNICK = 401,
     ERR_NICKCOLLISION = 436,
     //:<server> 421 <nickname> <command> :Unknown command
