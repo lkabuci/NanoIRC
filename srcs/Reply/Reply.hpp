8c268430bf63e410f3f6111dd124ea3039ee140e
Issam Abkadri <iabkadri@student.1337.ma>
2024-01-20 23:17:26 +0100

    add errors description

--- srcs/Reply/Reply.hpp
+++ srcs/Reply/Reply.hpp
@@ -17,28 +17,43 @@ namespace ERROR_CODES {
 enum CODES {
     //:<server> 462 <nickname> :You may not reregisted
     ERR_ALREADYREGISTRED = 462,
+
     //:<server> 461 <nickname> <command> :Not enough parameters
     ERR_NEEDMOREPARAMS = 461,
     ERR_NONICKNAMEGIVEN = 431,
+
     //:<server> 432 <nickname> :Erroneous nickname
     ERR_ERRONEUSNICKNAME = 432,
+
     //:<server> 433 <nickname> :Nickname is already in use
     ERR_NICKNAMEINUSE = 433,
+
     //:<server> 403 <nickname> <channel> :No such channel
     ERR_NOSUCHCHANNEL = 403,
+
+    //:<server> 473 <nickname> <channel> :Cannot join channel (+i)
     ERR_INVITEONLYCHAN = 473,
+
+    //:<server> 475 <nickname> <channel> :Cannot join channel (+k)
     ERR_BADCHANNELKEY = 475,
+
     //:<server> 411 <nickname> :No recipient given (PRIVMSG/NOTICE)
     ERR_NORECIPIENT = 411,
+
     //:<server> 412 <nickname> :No text to send
     ERR_NOTEXTTOSEND = 412,
+
     //:<server> 407 <nickname> <command> :Too many targets
     ERR_TOOMANYTARGETS = 407,
+
     //:<server> 401 <nickname> <target> :No such nickname
     ERR_NOSUCHNICK = 401,
+
     ERR_NICKCOLLISION = 436,
+
     //:<server> 421 <nickname> <command> :Unknown command
     ERR_UNKNOWNCOMMAND = 401,
+
     // :<server> 451 <nickname> :You have not registered
     ERR_NOTREGISTERED = 451,
 };
