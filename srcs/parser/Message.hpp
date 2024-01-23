0f7f6fa3b6518b168f260e3562cd806ede6ad8c7
Issam Abkadri <iabkadri@student.1337.ma>
2024-01-23 11:36:36 +0100

    remove MAX_MSG_LEN enum

--- srcs/parser/Message.hpp
+++ srcs/parser/Message.hpp
@@ -28,7 +28,7 @@ class Message {
     Message(const Message&);
     Message& operator=(const Message&);
 
-    enum { MAX_MSG_LEN = 512, MAX_PARAMS = 15, CMDS_NBR = 12 };
+    enum { MAX_PARAMS = 15, CMDS_NBR = 12 };
 
     static TYPES::TokenType _commandTypes[CMDS_NBR];
     static std::string      _commandsStr[CMDS_NBR];
