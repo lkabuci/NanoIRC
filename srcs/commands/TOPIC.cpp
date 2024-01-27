0a58d44bb2567c4869ea0b76db471e3bf6663cc1
Abderrahim Said <absaid@e3r11p8.1337.ma>
2024-01-27 10:11:27 +0100

    need success rpl

--- srcs/commands/TOPIC.cpp
+++ srcs/commands/TOPIC.cpp
@@ -1,25 +1,37 @@
 #include "TOPIC.hpp"
 #include <cstddef>
-#include <stdexcept>
 #include <vector>
 
-void send1(int fd, const std::string& nick) {
+void sendErr1(int fd, const std::string& nick) {
     std::string msg = std::string(":") + Reactor::getInstance().getServerIp() +
                       std::string(" 461 ") + nick +
                       " TOPIC :Not enough parameters\r\n";
     send(fd, msg.c_str(), msg.size(), 0);
 }
 
-void send3(int fd, const std::string& nick, const std::string& channel) {
+void sendErr2(int fd, const std::string& nick, const std::string& channel) {
     std::string msg = std::string(":") + Reactor::getInstance().getServerIp() +
                       std::string(" 403 ") + nick + " #" + channel +
                       " :No such channel\r\n";
     send(fd, msg.c_str(), msg.size(), 0);
 }
-void send4(int fd, const std::string& nick, const std::string& channel) {
+void sendErr3(int fd, const std::string& nick, const std::string& channel) {
     std::string msg = std::string(":") + Reactor::getInstance().getServerIp() +
                       std::string(" 442 ") + nick + " #" + channel +
                       " :You're not on that channel\r\n";
+    send(fd, msg.c_str(), msg.size(), 0);
+}
+void sendErr4(int fd, const std::string& nick, const std::string& channel) {
+    std::string msg = std::string(":") + Reactor::getInstance().getServerIp() +
+                      std::string(" 331 ") + nick + " #" + channel +
+                      " :No topic is set.\r\n";
+    send(fd, msg.c_str(), msg.size(), 0);
+}
+
+void sendErr5(int fd, const std::string& nick, const std::string& channel) {
+    std::string msg = std::string(":") + Reactor::getInstance().getServerIp() +
+                      std::string(" 482 ") + nick + " #" + channel +
+                      " :You're not channel operator\r\n";
     send(fd, msg.c_str(), msg.size(), 0);
 }
 
@@ -29,22 +41,28 @@ void TOPIC::execute(Client*                         client,
     std::string              topic;
     if (tmp.size() < 1)
         return (
-            send1(client->getSockfd(), client->getUserInfo().getNickname()));
+            sendErr1(client->getSockfd(), client->getUserInfo().getNickname()));
     if (tmp[0][0] != '#')
-        return (send3(client->getSockfd(), client->getUserInfo().getNickname(),
-                      tmp[0].substr(1)));
+        return (sendErr2(client->getSockfd(),
+                         client->getUserInfo().getNickname(),
+                         tmp[0].substr(1)));
     tmp[0].erase(0, 1);
     if (!TChannels::exist(tmp[0]))
-        return (send3(client->getSockfd(), client->getUserInfo().getNickname(),
-                      tmp[0]));
+        return (sendErr2(client->getSockfd(),
+                         client->getUserInfo().getNickname(), tmp[0]));
     Channel tmpChannel = TChannels::channel(tmp[0]);
     if (!tmpChannel.exist(client))
-        return (send4(client->getSockfd(), client->getUserInfo().getNickname(),
-                      tmpChannel.name()));
+        return (sendErr3(client->getSockfd(),
+                         client->getUserInfo().getNickname(),
+                         tmpChannel.name()));
     tmp.erase(tmp.begin());
     if (tmp.empty()) {
         topic = tmpChannel.getTopic();
-        return;
+        if (topic.empty())
+            return (sendErr4(client->getSockfd(),
+                             client->getUserInfo().getNickname(),
+                             tmpChannel.name()));
+        return; // rpl
     } else {
         if (tmp[0][0] != ':') {
             topic = tmp[0];
@@ -57,9 +75,11 @@ void TOPIC::execute(Client*                         client,
             topic.pop_back();
         }
         if (tmpChannel.flagIsSet(client, MEMBER_PERMISSION::OPERATOR) ||
-            tmpChannel.modeIsSet(CHANNEL_MODE::SET_TOPIC))
-            tmpChannel.setTopic(topic);
-        else
-            throw std::runtime_error("cannot set topic");
+            tmpChannel.modeIsSet(CHANNEL_MODE::SET_TOPIC)) {
+            tmpChannel.setTopic(topic); // rpl
+        } else
+            return (sendErr5(client->getSockfd(),
+                             client->getUserInfo().getNickname(),
+                             tmpChannel.name()));
     }
 }
