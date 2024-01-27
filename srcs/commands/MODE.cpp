f20cf4de61737da02f4404441832a67a9230c4f9
Abderrahim Said <absaid@e1r7p13.1337.ma>
2024-01-27 17:45:01 +0100

    fix : pass in mode

--- srcs/commands/MODE.cpp
+++ srcs/commands/MODE.cpp
@@ -90,8 +90,18 @@ void handleKey(bool state, char c, std::vector<std::string>& tmp,
                          client->getUserInfo().getNickname(), channel.name(),
                          str + "k"));
     }
-    std::string pass = tmp[0];
-    tmp.erase(tmp.begin());
+    std::string pass;
+    if (tmp[0][0] != ':') {
+        pass = tmp[0];
+        tmp.erase(tmp.begin());
+    } else {
+        tmp[0].erase(0, 1);
+        for (size_t i = 0; i < tmp.size(); i++) {
+            pass += tmp[i];
+            pass += " ";
+        }
+        pass.pop_back();
+    }
     if (!state && channel.modeIsSet(CHANNEL_MODE::SET_KEY)) {
         if (pass != channel.getPassword())
             return (sendErr8(client->getSockfd(),
@@ -267,7 +277,7 @@ void MODE::execute(Client* client, const std::vector<std::string>& parameters) {
     if (!TChannels::exist(tmp[0]))
         return (sendErr3(client->getSockfd(),
                          client->getUserInfo().getNickname(), tmp[0]));
-    Channel tmpChannel = TChannels::channel(tmp[0]);
+    Channel& tmpChannel = TChannels::channel(tmp[0]);
     tmp.erase(tmp.begin());
     if (!tmpChannel.exist(client))
         return (sendErr4(client->getSockfd(),
